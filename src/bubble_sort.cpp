6b818e14407e6c67ab9e5f44a6c3d0bdee3c2e4f
Minjie <mjopenglsdl@gmail.com>
2019-11-27 22:31:41 +0800

    initial commit

---
+++ src/bubble_sort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void print_array(int ary[], size_t size)
+{
+    for (size_t i = 0; i < size; ++i){
+        cout<<" "<<ary[i];
+    }
+    cout<<" "<<endl;
+}
+
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubble_sort(int ary[], size_t size)
+{
+    for(size_t i = 0; i < size-1; i++)
+    {
+        for (size_t j = 0; j < size - i -1; j++)
+        {
+            if(ary[j]>ary[j+1]){
+                swap(ary[j], ary[j+1]);
+            }
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    int input[] = {2, 7, 9, 5, 8, 1};
+    size_t ary_size = sizeof(input)/sizeof(input[0]);
+
+    cout<<"BEFORE: "<<endl;
+    print_array(input, ary_size);
+    
+    bubble_sort(input, ary_size);
+
+    cout<<"\nAFTER: "<<endl;
+    print_array(input, ary_size);
+
+return 0;
+}
